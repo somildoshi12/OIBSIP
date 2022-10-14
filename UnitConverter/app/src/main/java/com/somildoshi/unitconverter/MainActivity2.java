994aaed22ba3e40adf18a519117f8b551d2b01cc
Somil <Somildoshi12@gmail.com>
2022-10-14 22:00:59 +0530

    6th commit of 14/10/22

--- UnitConverter/app/src/main/java/com/somildoshi/unitconverter/MainActivity2.java
+++ UnitConverter/app/src/main/java/com/somildoshi/unitconverter/MainActivity2.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
     Window window;
+    Button len, spd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +25,25 @@ public class MainActivity2 extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.black));
         }
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        len = findViewById(R.id.btn1);
+        len.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
+            public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
                 startActivity(intent);
-                finish();
             }
-        },3000);
+        });
+
+        spd = findViewById(R.id.btn2);
+        spd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity2.this,MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
